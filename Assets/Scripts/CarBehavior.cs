844a30b348419f682f4ff6668794bfc6379d4fbb
Bill Jackson <bjackson@cakewalk.com>
2018-01-23 16:25:36 -0500

    Collisions, score, safety, road units

--- Assets/Scripts/CarBehavior.cs
+++ Assets/Scripts/CarBehavior.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class CarBehavior : MonoBehaviour {
 	public float speed = 3;
+	public Transform safeSpriteTransform;
+
+	private bool isSafe = false;
 
 	void Update () {
-		transform.position += transform.up * speed * Time.deltaTime;
+		if (!GameManager.instance.GetGameOver())
+			transform.position += transform.up * speed * Time.deltaTime;
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Wall") {
+			if (isSafe) {
+				SetSafe (false);
+			} else {
+				KillCar ();
+			}
+		}
+	}
+
+	public void SetSafe(bool safe)
+	{
+		safeSpriteTransform.gameObject.SetActive (safe);
+		isSafe = safe;
+
+	}
+
+	private void KillCar()
+	{
+		transform.localScale = Vector3.one * .5f;
+		GameManager.instance.SetGameOver();
 	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public Text scoreOutput;
+
+	private int score;
+	private bool isGameOver;
+
+	public Action OnGameOver;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			// Bail if an instance already exists
+			DestroyObject(this);
+		}
+	}
+	private void Start()
+	{
+		isGameOver = false;
+		score = 0;
+		StartCoroutine (RunMeterCounter ());
+	}
+
+	public void SetGameOver()
+	{
+		isGameOver = true;
+	}
+	public bool GetGameOver()
+	{
+		return isGameOver;
+	}
+
+	private IEnumerator RunMeterCounter()
+	{
+		while (!isGameOver) {
+			yield return new WaitForSeconds(.5f);
+			score += 1;
+			scoreOutput.text = score.ToString() + " m";
+
+		}
+	}
+
+}
